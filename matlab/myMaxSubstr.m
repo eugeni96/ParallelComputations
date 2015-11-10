f4d78454cc0e159a2e76874f1da73eb0cea06eef
Eugeni96 <e.a.tseitlin@gmail.com>
2015-11-10 22:53:39 +0300

    add myMaxSubstring

---
+++ matlab/myMaxSubstr.m
@@ -0,0 +1,17 @@
+function max = myMaxSubstr(S, T)
+L=zeros(length(S), length(T));
+u = 1; v = 1;
+for i = 1:length(S)
+    for j = 1:length(T)
+        if S(i) == T(j)
+            L(i+1,j+1) = L(i,j) + 1;
+            if (L(i + 1, j + 1) > L(u, v))                 
+               u = i + 1;
+               v = j + 1;
+            end
+        end
+    end
+    max = S(u - L(u,v):L(u,v));
+end
+            
+            
